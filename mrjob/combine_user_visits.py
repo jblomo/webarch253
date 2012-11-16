26b7f9a2698ae02f02895e90c41eaaa1b1830a09
Jim Blomo <jim.blomo+github@gmail.com>
2012-11-15 22:14:45 -0800

    Homework

---
+++ mrjob/combine_user_visits.py
@@ -0,0 +1,47 @@
+"""Convert user visits from separate lines to the same line, so they can be
+processed by MapReduce/mrjob.
+
+This program takes a CSV data file and outputs a new CSV data file with the
+user ID appended to each Vote/Visit.  Run like so:
+
+    python mrjob/combine_user_visits.py mrjob/anonymous-msweb.data > mrjob/user-visits_msweb.data
+"""
+    
+import csv
+import fileinput
+from sys import stdout
+
+
+def csv_readline(line):
+    """Given a sting CSV line, return a list of strings."""
+    for row in csv.reader([line]):
+        return row
+
+
+def main():
+    """This function iterates through an input file linearly, keeping track of
+    the current user.  When we see a Vote/Visit line, we append the current
+    user to the cell.  Then we write all lines out again (updated or not).
+    """
+
+    csv_writer = csv.writer(stdout)
+    current_user = None
+
+    for line in fileinput.input():
+        cell = csv_readline(line)
+        if cell[0] == 'C':
+            ###
+            # FILL IN:
+            # What should we update when we see a new 'C' row?
+            ##/
+        elif cell[0] == 'V':
+            ###
+            # FILL IN:
+            # What should we update when we see a new 'V' row?
+            ##/
+
+        csv_writer.writerow(cell)
+
+
+if __name__ == '__main__':
+    main()
---
+++ mrjob/top_pages.py
@@ -0,0 +1,34 @@
+"""Find Vroots with more than 400 visits.
+
+This program will take a CSV data file and output tab-seperated lines of
+
+    Vroot -> number of visits
+
+To run:
+
+    python mrjob/top_pages.py mrjob/anonymous-msweb.data
+"""
+
+from mrjob.job import MRJob
+from combine_user_visits import csv_readline
+
+class TopPages(MRJob):
+
+    def mapper(self, line_no, line):
+        """Extracts the Vroot that was visited"""
+        cell = csv_readline(line)
+        if cell[0] == 'V':
+            yield ### FILL IN
+                  # What  Key, Value  do we want to output?
+
+    def reducer(self, vroot, visit_counts):
+        """Sumarizes the visit counts by adding them together.  If total visits
+        is more than 400, yield the results"""
+        total = ### FILL IN
+                # How do we calculate the total visits from the visit_counts?
+        if total > 400:
+            yield ### FILL IN
+                  # What  Key, Value  do we want to output?
+        
+if __name__ == '__main__':
+    TopPages.run()
