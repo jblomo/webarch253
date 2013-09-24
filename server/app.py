e97df6a59cd9eba724a3c4a4eb6947941c794175
Jim Blomo <jim.blomo+github@gmail.com>
2013-09-23 17:24:53 -0700

    HTTP Data slides and HW

--- server/app.py
+++ server/app.py
@@ -29,14 +29,51 @@ def home():
 
 ###
 # Wiki Resource:
-# Only support GET method, redirects to the English Wikipdia
+# GET method will redirect to the resource stored by PUT, but default: Wikipedia.org
+# POST/PUT method will update the redirect destination
 ###
 @app.route('/wiki', methods=['GET'])
-def wiki():
+def wiki_get():
     """Redirects to wikipedia."""
     destination = "http://en.wikipedia.org"
     app.logger.debug("Redirecting to " + destination)
     return flask.redirect(destination)
+
+@app.route("/wiki", methods=['PUT', 'POST'])
+def wiki_put():
+    """Set or update the URL to which this resource redirects to. Uses the
+    `url` key to set the redirect destination."""
+    wikipedia = request.form.get('url', "http://en.wikipedia.org")
+    db['wiki'] = wikipedia
+    return "Stored wiki => " + wikipedia
+
+###
+# i253 Resource:
+# Information on the i253 class. Can be parameterized with `relationship`,
+# `name`, and `adjective` information
+#
+# TODO: The representation for this resource is broken. Fix it!
+# Set the correct MIME type to be able to view the image in your browser
+##/
+@app.route('/i253')
+def i253():
+    """Returns a PNG image of madlibs text"""
+    relationship = request.args.get("relationship", "friend")
+    name = request.args.get("name", "Jim")
+    adjective = request.args.get("adjective", "fun")
+
+    resp = flask.make_response(
+            check_output(['convert', '-size', '600x400', 'xc:transparent',
+                '-font', '/usr/share/fonts/thai-scalable/Waree-BoldOblique.ttf',
+                '-fill', 'black',
+                '-pointsize', '32',
+                '-draw',
+                  "text 10,30 'My %s %s said i253 was %s'" % (relationship, name, adjective),
+                'png:-']), 200);
+    # Comment in to set header below
+    # resp.headers['Content-Type'] = '...'
+
+    return resp
 
 
 if __name__ == "__main__":
